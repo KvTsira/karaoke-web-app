cf7a0787c310a825e836c0fdfc76b487d4006311
dannyrgd <danny.ramirezgd@gmail.com>
2022-01-03 12:20:58 -0500

    fetch function pulls data and consoles

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,1 +1,14 @@
-console.log("hello");
+var search = document.getElementById("search-field");
+var accessToken = config.genius_api_token;
+var getArtistSongs = function(){
+    var apiUrl = "https://api.genius.com/search?q=Kendrick%20Lamar&access_token=" + accessToken
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data);
+            })
+        }
+    })
+}
+
+getArtistSongs();
