cf3f62a9bcc8cd5df9d6d795ac86fd431b02c093
dannyrgd <danny.ramirezgd@gmail.com>
2022-01-03 13:07:57 -0500

    search button handler works with api and appends data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,14 +1,39 @@
 var search = document.getElementById("search-field");
-var accessToken = config.genius_api_token;
-var getArtistSongs = function(){
-    var apiUrl = "https://api.genius.com/search?q=Kendrick%20Lamar&access_token=" + accessToken
+var searchBtn = document.getElementById("search");
+var songList = document.getElementById("song-list")
+var accessToken = "UAzdTm4zgiU6lk_3xsyqJf9iI_5bv5yIDVbgLld44Y3U90kuq2IR3uoBZ6j66XbT";
+
+
+var getArtistSongs = function(artist){
+    var apiUrl = "https://api.genius.com/search?q=" + artist + "&access_token=" + accessToken
     fetch(apiUrl).then(function(response){
         if(response.ok){
             response.json().then(function(data){
-                console.log(data);
+                console.log(data)
+                songList.innerHTML = "";
+                if(data.response.hits.length === 0){
+                    songList.textContent = "Please Enter A Valid Artist"
+                }
+                for (let i=0; i<data.response.hits.length; i++){
+                    var songItemEl = document.createElement("li")
+                    var title = data.response.hits[i].result.title;
+                    var artist = data.response.hits[i].result.artist_names;
+                    var songEl = document.createElement("a");
+                    songEl.href = "#"
+                    songEl.textContent = title + " by " + artist
+                    songItemEl.appendChild(songEl);
+                    songList.appendChild(songItemEl);
+                }
             })
+        } else {
+            songList.textContent = "Please Enter Valid Artist"
         }
     })
 }
 
-getArtistSongs();
+var searchButtonHandler = function(event){
+    event.preventDefault();
+    var userInput = search.value
+    getArtistSongs(userInput);
+}
+searchBtn.addEventListener("click", searchButtonHandler)
