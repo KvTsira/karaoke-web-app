4ff04e5dc4b18bee0f4471aba567036f83573a54
KvTsira <kvtsira@gmail.com>
2022-01-04 23:47:42 -0500

    Display the results in a table grid

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,10 +1,21 @@
 var search = document.getElementById("search-field");
 var searchBtn = document.getElementById("search");
-var songList = document.getElementById("song-list")
+//var songList = document.getElementById("song-list")
 var accessToken = "UAzdTm4zgiU6lk_3xsyqJf9iI_5bv5yIDVbgLld44Y3U90kuq2IR3uoBZ6j66XbT";
 var searchHistory = document.getElementById("search-history");
+var txtResult=document.getElementById("result")
 var artistSearches = [];
 
+
+var tbodyEl = document.querySelector('tbody');
+
+//removes the rows from the table
+var removeRows = function() {
+    while(tbodyEl.hasChildNodes())
+    {
+        tbodyEl.removeChild(tbodyEl.firstChild);
+    }
+}
 
 var getArtistSongs = function(artist){
     //call api based on search terms
@@ -13,10 +24,10 @@ var getArtistSongs = function(artist){
         if(response.ok){
             response.json().then(function(data){
                 //delete any left over text
-                songList.innerHTML = "";
+                //songList.innerHTML = "";
                 //if the artist or song returns no hits display message
                 if(data.response.hits.length === 0){
-                    songList.textContent = "Please Enter A Valid Artist"
+                    //songList.textContent = "Please Enter A Valid Artist"
                 }
                 //run for loop and iterate through song list and display them in a list as buttons
                 for (let i=0; i<data.response.hits.length; i++){
@@ -24,15 +35,43 @@ var getArtistSongs = function(artist){
                     //pull data from response and create two variables 
                     var title = data.response.hits[i].result.title;
                     var artist = data.response.hits[i].result.artist_names;
-                    var songEl = document.createElement("button");
-                    songEl.textContent = title + " by " + artist
-                    songItemEl.appendChild(songEl);
-                    songList.appendChild(songItemEl);
+                    //var songEl = document.createElement("button");
+                    //songEl.textContent = title + " by " + artist
+                    //songItemEl.appendChild(songEl);
+                    //songList.appendChild(songItemEl);
+
+                    tbodyEl.innerHTML += `
+                        <tr>
+                            <td class=".row-data">${artist}</td>
+                            <td class=".row-data">${title}</td>
+                            <td><button class="selectBtn btn-primary">Select</button></td>
+                        </tr>
+                    `;
                 }
             })
         } 
     })
 }
+
+
+//add a function that responses to click events
+function onselectRow(e){
+    //check if the button is clicked
+    if(!e.target.classList.contains('selectBtn')){
+        return;
+    }
+    
+    //get the row number
+    var row_num = parseInt($(e.target).parent().parent().index());
+    var oCells=tbodyEl.rows.item(row_num).cells;
+
+    //write the selected item artist and title to the text box
+    txtResult.innerHTML="Artist: " + oCells.item(0).innerHTML + "; Title: " + oCells.item(1).innerHTML ; 
+};
+
+//add an event to table rows buttons
+tbodyEl.addEventListener('click', onselectRow);
+
 
 var searchButtonHandler = function(event){
     event.preventDefault();
@@ -47,6 +86,8 @@ var searchButtonHandler = function(event){
     }
     //on click grab data from search bar
     var userInput = search.value
+    //clear the table
+    removeRows();
     //run that data as a parameter through get artist songs function
     getArtistSongs(userInput);
     //push the input into artist searches array
@@ -76,6 +117,8 @@ var searchHistoryBtnHandler = function (event){
         event.preventDefault();
         //take the text content of the button and create a variable for it
         var input = event.target.textContent
+        //clear the table
+        removeRows();
         //pass the input as a parameter for getArtistSongs
         getArtistSongs(input);
     }
